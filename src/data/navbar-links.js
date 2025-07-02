1c03230885b8b44b26461030dba2031814dff730
Man Patel <man.mp07@gmail.com>
2025-07-02 12:48:49 +0530

    Navbar created

--- src/data/navbar-links.js
+++ src/data/navbar-links.js
@@ -1,26 +1,26 @@
 export const NavbarLinks = [
-  {
-    title: "Home",
-    path: "/",
-  },
-  {
-    title: "Catalog",
-    path: '/catalog',
-  },
-  {
-    title: "Project",
-    path: "/project",
-  },
-  {
-    title: "About Us",
-    path: "/about",
-  },
-  {
-    title: "Contact Us",
-    path: "/contact",
-  },
-  {
-    title:"Rate Us",
-    path:"/rateus",
-  }
+    {
+        title: "Home",
+        path: "/",
+    },
+    {
+        title: "Catalog",
+        //path: '/catalog',
+    },
+    {
+        title: "Project",
+        path: "/project",
+    },
+    {
+        title: "About Us",
+        path: "/about",
+    },
+    {
+        title: "Contact Us",
+        path: "/contact",
+    },
+    {
+        title: "Rate Us",
+        path: "/rateus",
+    }
 ];
